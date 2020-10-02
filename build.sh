e00cec1ba8ac8647cfbfa373ba20256bcfdf1e7e
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-10-02 20:41:58 -0300

    Package add

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-#github_version=$(cat github_version.txt)
-github_version=1.19.2
+github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
 
--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#github_version=$(cat github_version.txt)
-github_version=1.19.2
+github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 LOCALPATH=$GOPATH/src/k8s.io/kubernetes/
 BINPATH=$GOPATH/src/k8s.io/kubernetes/_output/local/bin/linux/ppc64le
